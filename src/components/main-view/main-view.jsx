b9bb482393a16f6a37f064ac91071a317443b23e
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-06-27 17:03:53 +0200

    includiung signup-view and login-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,38 +1,65 @@
 import { useEffect, useState } from "react";
-import { MovieCard} from "../movie-card/movie-card"
-import { MovieView} from "../movie-view/movie-view"
+import { MovieCard } from "../movie-card/movie-card"
+import { MovieView } from "../movie-view/movie-view"
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
   const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState (null);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
-const JsonApi = "https://api.jsonbin.io/v3/qs/6495b4b1b89b1e2299b3e32d";
-const herokuApi = "https://guarded-peak-19726.herokuapp.com/movies";
+
+
+  const JsonApi = "https://api.jsonbin.io/v3/qs/6495b4b1b89b1e2299b3e32d";
+  const herokuApi = "https://guarded-peak-19726.herokuapp.com/movies";
 
   useEffect(() => {
-    fetch(JsonApi)
-    .then ((response) => response.json())
-    .then ((data) => {
-      console.log("moviedata", data);
-      const movieFromApi = data.record.map((movie) => {
-        return {
-          id: movie._id.__$MSCoid,
-          Poster: movie.Poster,
-          Title: movie.Title,
-          Genre: movie.Genre[0].name,
-          Description: movie.Description,
-          Director: movie.Director.Name
-        };
-      });
-      setMovies(movieFromApi);
-    });
-  }, []);
+    if (!token) return;
 
-  
- 
+    fetch(herokuApi, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then((movies) => {
+        setMovies(movies);
+
+      });
+  }, [token]);
+
+  //   return {
+  //     id: movie._id.__$MSCoid,
+  //     Poster: movie.Poster,
+  //     Title: movie.Title,
+  //     Genre: movie.Genre[0].name,
+  //     Description: movie.Description,
+  //     Director: movie.Director.Name
+  //   };
+  // });
+  // setMovies(movieFromApi);
+
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
+
   if (selectedMovie) {
     return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null) } />
+      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
     );
   }
 
@@ -43,14 +70,17 @@ const herokuApi = "https://guarded-peak-19726.herokuapp.com/movies";
   return (
     <div>
       {movies.map((movie) => (
-       <MovieCard
-       key={movie.id}
-       movie={movie}
-       onMovieClick={(newSelectedMovie) => {
-         setSelectedMovie(newSelectedMovie);
-       }}
-     />
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
       ))}
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>
+        Logout
+      </button>
     </div>
-          );
-    };
+  );
+};
