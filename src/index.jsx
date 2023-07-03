adbd51502a2f62cbec61054c1589bc10f034cf71
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-07-03 09:55:23 +0200

    adding a container tag as parent for the mainview

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,14 @@ import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 
 import "./index.scss";
+import { Container } from "react-bootstrap";
 
 const App = () => {
- return <MainView />;
+ return (
+    <Container>
+        <MainView />
+    </Container>
+ )
 };
 
 const container = document.querySelector("#root");
