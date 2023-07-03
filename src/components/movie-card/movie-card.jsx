a36a803bce79638279ef99d9342f94009f74aca9
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-07-03 09:54:19 +0200

    adding bootstrap card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,18 @@
-export const MovieCard = ({movie, onMovieClick}) => {
-  return <div
-    onClick = {()=> {
-      onMovieClick(movie);
-    }} >
-    {movie.Title} 
-    </div>;
-};
+import Card from 'react-bootstrap/Card';
 
+export const MovieCard = ({movie, onMovieClick}) => {
+  return (
+    <Card className= "h-100" >
+      <Card.Img 
+      variant="top" 
+      src= {movie.Poster} 
+      onClick = {()=> { onMovieClick(movie); }}
+      />
+      <Card.Body>
+        <Card.Title> {movie.Title} </Card.Title>
+        <Card.Text> {movie.Description} </Card.Text>
+      </Card.Body>
+    </Card>
+  ); 
+}
 
