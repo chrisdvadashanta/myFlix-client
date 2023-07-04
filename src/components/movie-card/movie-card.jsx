ca899f0117c5706d1f4207fa6feca5c49ed16829
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-07-04 16:32:45 +0200

    changing button alignment

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,7 @@
 import Card from "react-bootstrap/Card";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
+import './movie-card.scss';
 
 export const MovieCard = ({ movie }) => {
   return (
@@ -10,11 +10,13 @@ export const MovieCard = ({ movie }) => {
         variant="top"
         src={movie.Poster}
       />
-      <Card.Body>
+      <Card.Body className="d-flex flex-column">
         <Card.Title> {movie.Title} </Card.Title>
         <Card.Text> {movie.Description} </Card.Text>
-        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
-          <Button variant="link">Open</Button>
+        <Link className="d-grid gap-2 mt-auto" to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button class="btn btn-primary " type="button">
+            Open
+          </Button>
         </Link>
       </Card.Body>
     </Card>
