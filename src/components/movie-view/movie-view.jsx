5a4f2713dbbb3e828a0fa7aa42548157f9f804f2
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-07-07 13:49:33 +0200

    adding favorite buttons

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,60 +8,70 @@ import { useParams } from "react-router";
 import "./movie-view.scss";
 import { Backend_API } from "../../utils/constant";
 
-export const MovieView = ({ movies, user, setUser, token}) => {
+export const MovieView = ({ movies, user, setUser, token }) => {
   const { movieId } = useParams();
-    const [ isFavorite, setIsFavorite ] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
 
-    useEffect(() => {
-       const isFavorited = user.favorites.includes(movieId)
-       setIsFavorite(isFavorited)
-    }, []);
-
-
+  useEffect(() => {
+    const isFavorited = user.favorites.includes(movieId);
+    setIsFavorite(isFavorited);
+  }, []);
   const movie = movies.find((m) => m.id === movieId);
 
-
-
   const removeFavorite = () => {
-    fetch(`${Backend_API}/users/:username/:movieId`, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        }
-    }).then((response) => {
+    fetch(`${Backend_API}/:username/movies/:Title`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
         if (response.ok) {
-            return response.json()
+          return response.json();
         }
-    }).then((data) => {
+      })
+      .then((data) => {
         setIsFavorite(false);
         localStorage.setItem("user", JSON.stringify(data));
         setUser(data);
-    })
-};
+      });
+  };
 
-const addToFavorite = () => {
-    fetch(`${Backend_API}/users/:username/:movieId`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        }
-    }).then((response) => {
+  const addToFavorite = () => {
+    fetch(`${Backend_API}/users/:username/movies/:Title`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
         if (response.ok) {
-            return response.json()
+          return response.json();
         }
-    }).then((data) => {
+      })
+      .then((data) => {
         setIsFavorite(true);
         localStorage.setItem("user", JSON.stringify(data));
         setUser(data);
-    })
-}
+      });
+  };
 
-    return (
+  return (
     <div>
-      <div align="center">
-        <img src={movie.Poster} className="movie-poster" />
+      <div className="image-container">
+      <img src={movie.Poster} className="movie-poster" />
+      <span className="button-container-a">
+        <Button className="favoriteMovieButton" onClick={addToFavorite}>
+          add ⭐
+        </Button>
+      </span>
+      <span className="button-container-b">
+        <Button className="removeFavoriteMovieButton" onClick={removeFavorite}>
+          remove ⭐
+        </Button>
+     </span>
       </div>
       <p />
       <Accordion defaultActiveKey="0">
@@ -73,28 +83,21 @@ const addToFavorite = () => {
         <Accordion.Item eventKey="1">
           <Accordion.Header> Director: {movie.Director.Name}</Accordion.Header>
           <Accordion.Body>
-            Name and Age
-            {movie.Director.Description}
+            {movie.Director.Bio}
           </Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="2">
           <Accordion.Header> Genre: {movie.Genre}</Accordion.Header>
-          <Accordion.Body>Description</Accordion.Body>
+          <Accordion.Body> ddd </Accordion.Body>
         </Accordion.Item>
       </Accordion>
       <p />
-        <Link to={`/`} className="d-grid gap-2">
-          <Button variant="primary" size="sm" >Back</Button>
-        </Link>
-
-      <Button className="favoriteMovieButton" onClick={addToFavorite}>
-        ⭐
-      </Button>
-      <Button className="removeFavoriteMovieButton" onClick={removeFavorite}>
-        ⭐
-      </Button>
-
+      <Link to={`/`} className="d-grid gap-2">
+        <Button variant="primary" size="sm">
+          Back
+        </Button>
+      </Link>
     </div>
   );
 };
