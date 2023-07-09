b78c2b2afc9f57fc034508d723c2d30946b2d73e
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-07-09 20:17:43 +0200

    re-arange the grid

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -11,9 +11,6 @@ import { Backend_API } from "../../utils/constant";
 import "./profile-view.scss";
 
 export const ProfileView = ({ user, movies, onLogout }) => {
-  console.log("user favorite", user.favorites);
-  console.log("user info", user);
-
   ///// useState to update User information
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -67,34 +64,93 @@ export const ProfileView = ({ user, movies, onLogout }) => {
   };
 
   return (
-    <Container >
-      <Row >
-        <Col >
-        <CardGroup>
-          <Card >
-            <Card.Body>
-              <Card.Title>{user.username}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                {user.email}
-              </Card.Subtitle>
-              <Button variant="primary">Contact</Button>
-            </Card.Body>
-          </Card>
-          <Card className="image-card" > 
-            <Card.Img
-              variant="top"
-              src="https://www.shutterstock.com/image-vector/user-icon-trendy-flat-style-600w-418179865.jpg"
-              alt="profile icon"
-            />
-          </Card>
-        </CardGroup>
+    <Container>
+      <Row className="row-1" >
+        <Col md={6} sm={12}>
+          <Row className="row-1-1" >
+            <CardGroup className="cardgroup-1">
+              <Card>
+                <Card.Body>
+                  <Card.Title>{user.username}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {user.email}
+                  </Card.Subtitle>
+                  <Button variant="primary">Contact</Button>
+                </Card.Body>
+              </Card>
+              <Card className="image-card">
+                <Card.Img
+                  variant="top"
+                  src="https://www.shutterstock.com/image-vector/user-icon-trendy-flat-style-600w-418179865.jpg"
+                  alt="profile icon"
+                />
+              </Card>
+            </CardGroup>
+          </Row>
+
+          <Row className="row-1-2">
+            <Form onSubmit={handleSubmit} className="floating-label-form">
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Username"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  placeholder="Username"
+                />
+              </FloatingLabel>
+
+              <FloatingLabel controlId="floatingPassword" label="Password">
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                />
+              </FloatingLabel>
+
+              <FloatingLabel controlId="floatingEmail" label="Email">
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+              </FloatingLabel>
+
+              <FloatingLabel controlId="floatingBirthday" label="Birthdate">
+                <Form.Control
+                  placeholder="08.05.1978"
+                  type="date"
+                  value={birthdate}
+                  onChange={(e) => setBirthdate(e.target.value)}
+                />
+              </FloatingLabel>
+
+              <p />
+              <div className="d-grid gap-2">
+                <Button
+                  type="submit"
+                  variant="primary"
+                  size="sm"
+                  className="form-button"
+                >
+                  Update your data
+                </Button>
+              </div>
+            </Form>
+          </Row>
         </Col>
-        <Col  >
-          <Carousel >
+
+        <Col md={6} sm={12}>
+          <Carousel className="movie-carousel">
             {favorites.map((movie) => {
               return (
-                <Carousel.Item key={movie.id}>
-                  <img
+                <Carousel.Item key={movie.id} >
+                  <img 
                     className="d-block w-100"
                     src={movie.Poster}
                     alt={movie.Title}
@@ -109,69 +165,9 @@ export const ProfileView = ({ user, movies, onLogout }) => {
           </Carousel>
         </Col>
       </Row>
-      <Row>
-        <Col md={12}>
-          <h1>User Settings </h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={6}>
-          <Form onSubmit={handleSubmit} className="floating-label-form">
-            <FloatingLabel
-              controlId="floatingInput"
-              label="Username"
-              className="mb-3"
-            >
-              <Form.Control
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Username"
-              />
-            </FloatingLabel>
 
-            <FloatingLabel controlId="floatingPassword" label="Password">
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-            </FloatingLabel>
-
-            <FloatingLabel controlId="floatingEmail" label="Email">
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-            </FloatingLabel>
-
-            <FloatingLabel controlId="floatingBirthday" label="Birthdate">
-              <Form.Control
-                placeholder="08.05.1978"
-                type="date"
-                value={birthdate}
-                onChange={(e) => setBirthdate(e.target.value)}
-              />
-            </FloatingLabel>
-
-            <p />
-            <div className="d-grid gap-2">
-              <Button
-                type="submit"
-                variant="primary"
-                size="sm"
-                className="form-button"
-              >
-                Update your data
-              </Button>
-            </div>
-          </Form>
-        </Col>
-        <Col md={6}>
-          <Button variant="primary" onClick={handleShow}>
+      <Row className="row-3">
+          <Button variant="primary" onClick={handleShow} className="delete-button">
             Delete your Profile
           </Button>
           <Modal show={show} onHide={handleClose}>
@@ -192,7 +188,6 @@ export const ProfileView = ({ user, movies, onLogout }) => {
               </Button>
             </Modal.Footer>
           </Modal>
-        </Col>
       </Row>
     </Container>
   );
