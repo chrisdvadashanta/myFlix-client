3fb343d4b65ce7967567e207d599923d9de85f15
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-07-10 22:48:40 +0200

    adding link to img for movieview

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,14 +5,16 @@ import { Link } from "react-router-dom";
 export const MovieCard = ({ movie }) => {
   return (
     <Card className="h-100">
-      <Card.Img
-        variant="top"
-        src={movie.Poster}
-      />
+      <a href={`/movies/${encodeURIComponent(movie.id)}`}>
+        <Card.Img variant="top" src={movie.Poster} />
+      </a>
       <Card.Body className="d-flex flex-column">
         <Card.Title> {movie.Title} </Card.Title>
         <Card.Text> {movie.Description} </Card.Text>
-        <Link className="d-grid gap-2 mt-auto" to={`/movies/${encodeURIComponent(movie.id)}`}>
+        <Link
+          className="d-grid gap-2 mt-auto"
+          to={`/movies/${encodeURIComponent(movie.id)}`}
+        >
           <Button className="btn btn-primary " type="button">
             Open
           </Button>
