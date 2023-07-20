b50f07d26ab421c980099a6b169cada05a6526cd
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-07-20 14:27:38 +0200

    adding cors code for allowedOrigins

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
 import { createRoot } from "react-dom/client";
-// import { MainView } from "./components/main-view/main-view";
-import MainView from "./components/main-view/main-view";
+import { MainView } from "./components/main-view/main-view";
+// import MainView from "./components/main-view/main-view";
 
 
 import "./index.scss";
 import { Container } from "react-bootstrap";
+
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234','myflixmovie1234.netlify.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const App = () => {
  return (
